67b7b1d730bb56a554024609e4b7148395ca5220
Keshav K <kkeshav1998@gmail.com>
2023-10-22 04:18:18 -0700

    stage 4

--- app/main.js
+++ app/main.js
@@ -5,15 +5,20 @@ console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
+    // console.log("1: " + data);
     data = data.toString("utf-8").split(" ");
-    console.log("1: " + data[1]);
     if (data[1] == "/") {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
-      socket.end();
+      socket.write("HTTP/1.1 200 OK\r\n");
+    } else if (data[1].startsWith("/echo/")) {
+      socket.write("HTTP/1.1 200 OK\r\n");
+      socket.write("Content-Type: text/plain\r\n");
+      socket.write("Content-Length: 3\r\n");
+      socket.write("\r\n");
+      socket.write(data[1].split("/")[2]);
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-      socket.end();
     }
+    socket.end();
   });
 
   socket.on("close", () => {
