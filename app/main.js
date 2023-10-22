010c82abcc93d13571b3d311e50c8172fbf59462
Keshav K <kkeshav1998@gmail.com>
2023-10-22 03:18:38 -0700

    stage 3

--- app/main.js
+++ app/main.js
@@ -4,9 +4,15 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
-  socket.on("data", () => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    socket.end();
+  socket.on("data", (data) => {
+    if (data.at(4) + data.at(5) === "79") {
+      // summation of ascii values of / : [slash space]
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+      socket.end();
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      socket.end();
+    }
   });
 
   socket.on("close", () => {
